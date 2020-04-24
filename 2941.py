78ae5d23661cf7548c46fe63825cc6422e23fec2
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-04-24 21:57:23 +0900

    Add 2941

---
+++ 2941.py
@@ -0,0 +1,4 @@
+# BOJ 2941 크로아티아 알파벳
+
+print(len(input().replace("dz=","1").replace("c=","1").replace("c-","1").replace("d-","1").replace("lj","1").replace("nj","1").replace("s=","1").replace("z=", "1")))
+
