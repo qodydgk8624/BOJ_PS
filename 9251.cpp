2846b682b26b0668193ffbf45b2886f31b0fbe15
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-04-27 22:29:07 +0900

    Update 9251, 9252

--- 9251.cpp
+++ 9251.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-int dp[1001][1001]={0};
+int dp[1001][1001];
 
 using namespace std;
 
@@ -18,11 +18,9 @@ int main()
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 	cin >> a >> b;
-	a.insert(a.begin(),0);
-	b.insert(b.begin(),0);
 	for(int i=1; i<=a.size(); i++)
 		for(int j=1; j<=b.size(); j++)
-			dp[i][j]=max(max(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]+(a[i]==b[j]));
+			dp[i][j]=max(max(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]+(a[i - 1]==b[j - 1]));
 			
-	printf("%d",dp[a.size()-1][b.size()-1]);
+	printf("%d",dp[a.size()][b.size()]);
 }
--- 9252.cpp
+++ 9252.cpp
@@ -1,44 +1,43 @@
-// BOJ - 9252
+// BOJ - 9252 LCS 2
 
 #include <iostream>
 #include <string>
+#include <utility>
 
-int dp[1001][1001]={0};
+#define mp make_pair
+
 
 using namespace std;
 
+
 string a,b;
 
-int max(int a,int b)
+int D[1002][1002]={0};
+
+int max(int o,int o2)
 {
-	return a>b?a:b;
+	return o>o2?o:o2;
 }
-int f(int i,int j)
+void f(int i,int j)
 {
-	if(!dp[i][j]) return 0;
-	a[i]==b[j]?f(i-1,j-1),putchar(a[i]):dp[i-1][j]>dp[i][j-1]?f(i-1,j):f(i,j-1);
+	if(!D[i][j]) return;
+	if(a[i]==b[j])
+	{
+		f(i-1,j-1);
+		putchar(a[i]);
+	}
+	else D[i-1][j]>D[i][j-1]?f(i-1,j):f(i,j-1);
 }
 int main()
 {
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
 	cin >> a >> b;
 	a.insert(a.begin(),0);
 	b.insert(b.begin(),0);
 	for(int i=1; i<=a.size(); i++)
 		for(int j=1; j<=b.size(); j++)
-			dp[i][j]=max(max(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]+(a[i]==b[j]));
+			D[i][j]=max(D[i-1][j],max(D[i][j-1],D[i-1][j-1]+(a[i]==b[j])));
 			
-	printf("%d\n",dp[a.size()-1][b.size()-1]);
+	cout << D[a.size()-1][b.size()-1] << endl;
 	
 	f(a.size()-1,b.size()-1);
-	
-	/*
-	puts("");
-	for(int i=1; i<a.size(); puts(""),i++)
-		for(int j=1; j<b.size(); j++)
-			printf("%d ",dp[i][j]);
-	*/
-	
-	
 }
