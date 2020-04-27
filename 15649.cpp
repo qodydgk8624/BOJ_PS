6c566da06c0897c6b71d3ff034381bcc4a6bfa12
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-04-27 11:34:56 +0900

    Add 15649, 15650

---
+++ 15649.cpp
@@ -0,0 +1,39 @@
+// BOJ - 15649 N과 M (1)
+
+#include <iostream>
+#define MAXN 8 + 1
+
+using namespace std;
+
+int n, m;
+
+int visited[MAXN] = {0, };
+int seq[MAXN] = {0, };
+
+void dfs(int cnt)
+{
+    if(cnt == m)
+    {
+        for(int i = 0; i < m; i++)
+            cout << seq[i] << ' ';
+        cout << '\n'; return;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(!visited[i])
+        {
+            visited[i] = 1;
+            seq[cnt] = i;
+            dfs(cnt + 1);
+            visited[i] = 0;
+        }
+    }
+}
+int main()
+{
+    cin.tie(0); ios::sync_with_stdio;
+    cin >> n >> m;
+
+    dfs(0);
+}
