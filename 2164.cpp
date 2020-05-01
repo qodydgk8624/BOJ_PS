37457e6e2e5b15b4e9e4b1a61230eb45c6543ffb
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-05-02 01:45:41 +0900

    Add 2164

---
+++ 2164.cpp
@@ -0,0 +1,22 @@
+// BOJ - 2164 카드2
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0); ios::sync_with_stdio(false);
+
+    int n; cin >> n;
+    queue<int> q;
+    for(int i=1; i<=n; i++) q.push(i);
+
+    while(q.size() != 1)
+    {
+        q.pop(); q.push(q.front()); q.pop();
+    }
+
+    cout << q.front();
+}
