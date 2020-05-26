aadee88618bd3af8cc6dd9d0723982681b0e727c
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-05-26 21:16:03 +0900

    Add 12015

---
+++ 12015.cpp
@@ -0,0 +1,32 @@
+// BOJ - 12015 가장 긴 증가하는 부분 수열 2
+
+#include <iostream>
+#include <vector>
+
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    ll n; cin >> n;
+    ll ans = 0; vector<int> v;
+
+    v.push_back(0);
+
+    for(int i = 0 ; i < n; i++)
+    {
+        ll t; cin >> t;
+        if(t > v.back())
+        {
+            ans++; v.push_back(t);
+        }
+        else
+        {
+            auto it = lower_bound(v.begin(), v.end(), t);
+            *it = t;
+        }
+    }
+
+    cout << ans;
+}
