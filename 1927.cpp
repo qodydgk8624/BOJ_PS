4641638edbc2dd7e20ced658643d219d3529696f
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-05-28 13:02:07 +0900

    Add 1927

---
+++ 1927.cpp
@@ -0,0 +1,33 @@
+// BOJ - 1927 최소 힙
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#define ll long long
+
+using namespace std;
+
+priority_queue<ll> pq;
+
+int main()
+{
+    cin.tie(0); ios::sync_with_stdio(false);
+
+    ll n; cin >> n;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        int t; cin >> t;
+        if(t == 0)
+        {
+            if(pq.size() == 0) cout << 0 << '\n';
+            else
+            {
+                cout << -pq.top() << '\n';
+                pq.pop();
+            }
+            
+        }
+        else pq.push(-t);
+    }
+}
